416256b21213ae415edc71f0b7c3d3aa71815e78
Catalin Andrei Cacuci <contact@catalin.sh>
2024-12-17 00:17:47 +0100

    Add taurpc

--- tf-app/src/App.tsx
+++ tf-app/src/App.tsx
@@ -1,15 +1,16 @@
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
-import { invoke } from "@tauri-apps/api/core";
 import "./App.css";
+import { createTauRPCProxy } from "./api/bindings";
+
+const taurpc = await createTauRPCProxy();
 
 function App() {
   const [greetMsg, setGreetMsg] = useState("");
   const [name, setName] = useState("");
 
   async function greet() {
-    // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
-    setGreetMsg(await invoke("greet", { name }));
+    setGreetMsg(await taurpc.greet(name));
   }
 
   return (
